845cfdcb9003337e889b5da6c2d24a3a14582f42
Danny Nguyen <duongcrseven@gmail.com>
2024-10-25 16:44:01 +1100

    Update two_sum.cpp

--- Solution/1. Two Sum/two_sum.cpp
+++ Solution/1. Two Sum/two_sum.cpp
@@ -1,2 +1,18 @@
-
+// --------------------------------------------------
+// ---------------Brute Force Solution---------------
+// --------------------------------------------------
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {}; // Return Blank if no Solution is found
+    }
+};
 
